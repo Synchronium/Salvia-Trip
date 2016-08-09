14cfdaed5325fac2ea527bd7cbf1b2ed444870be
John Clarke <john.clarke@gmail.com>
2016-08-09 11:50:55 +0100

    Added auto routs for users, reports, comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('u', 'UserController');
+Route::resource('e', 'ReportController');
+Route::resource('c', 'CommentController');
